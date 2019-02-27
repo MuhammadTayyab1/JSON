4618d0fef1c0cf0786c4e27a0accbcd5cb77d07a
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-02-27 10:32:24 -0800

    json get and post

---
+++ Get_post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Collections.Specialized;
+
+namespace Examples.System.Net
+{
+
+    public class WebRequestPostExample
+    {
+        public static void Main()
+        {
+            using (var wb = new WebClient())
+            {
+                var data = new NameValueCollection();
+                data["username"] = "myUser";
+                data["password"] = "myPassword";
+
+                var response = wb.UploadValues("https://jsonplaceholder.typicode.com/posts", "POST", data);
+                string responseInString = Encoding.UTF8.GetString(response);
+                Console.WriteLine(responseInString);
+            }
+
+            using (var wb = new WebClient())
+            {
+                var response = wb.DownloadString("https://jsonplaceholder.typicode.com/todos/1");
+                Console.WriteLine(response);
+            }
+
+        }
+    }
+}
