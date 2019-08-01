fd0f1408b1b524fe8f96d39a40396576976967e3
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-08-01 13:46:52 -0700

    Update Get_post.cs

--- Get_post.cs
+++ Get_post.cs
@@ -17,10 +17,12 @@ namespace Examples.System.Net
                 data["username"] = "myUser";
                 data["password"] = "myPassword";
 
-                // Json get and post
+                // Json get and post 
+                
                 var response = wb.UploadValues("https://jsonplaceholder.typicode.com/posts", "POST", data);
                 string responseInString = Encoding.UTF8.GetString(response);
                 Console.WriteLine(responseInString);
+                
             }
 
             using (var wb = new WebClient())
